a42f6063c6f7f6cdf42b173d3292ec311288b1e7
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-02-04 16:35:18 +0530

    Added:Test case in which the invoice generator should return minimum fare

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -11,4 +11,13 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare,0.0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+    }
+
 }
