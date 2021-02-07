162fc1d582615cb0a723b4a881d8590b2f4a6681
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-02-07 17:35:24 +0530

    Refactored:Test Case to return invoice summary according to categories of rides

--- src/main/java/InvoiceService.java
+++ src/main/java/InvoiceService.java
@@ -1,23 +1,14 @@
 public class InvoiceService {
-    private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final double MINIMUM_FARE = 5;
-    private  final RideRepository rideRepository;
+      private  RideRepository rideRepository;
 
-    public InvoiceService() {
-        this.rideRepository = new RideRepository();
+    public void  setRideRepository(RideRepository rideRepository) {
+        this.rideRepository = rideRepository;
     }
-
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        return Math.max(totalFare, MINIMUM_FARE);
-    }
-
 
     public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance,ride.time);
+            totalFare += ride.cabRide.calcCostOfCabRide(ride);
         }
         return new InvoiceSummary(rides.length,totalFare);
     }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -3,53 +3,34 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    Ride[] rides = null;
     InvoiceService invoiceService = null;
+    InvoiceSummary expectedInvoiceSummary = null;
+    private RideRepository rideRepository = null;
+
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         invoiceService = new InvoiceService();
-
+        rideRepository = new RideRepository();
+        invoiceService.setRideRepository(rideRepository);
+        rides = new Ride[] {
+                new Ride(CabRide.NORMAL,2.0,5),
+                new Ride(CabRide.PREMIUM,0.1,1)
+        };
+        expectedInvoiceSummary = new InvoiceSummary(2,45.0);
     }
 
-    @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        double distance = 2.0;
-        int time = 5;
-        double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(25, fare,0.0);
-    }
-    @Test
-    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-        double distance = 0.1;
-        int time = 1;
-        double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(5, fare,0.0);
-    }
     @Test
     public void givenMultipleRideShouldReturnInvoiceSummary() {
-
-        Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1),
-        };
         InvoiceSummary summary = invoiceService.calculateFare(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
     @Test
     public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
         String userId = "a@b.com";
-        Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1),
-        };
         invoiceService.addRides(userId, rides);
-        Ride[] rides1 = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1),
-        };
-        invoiceService.addRides(userId, rides1);
         InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 
